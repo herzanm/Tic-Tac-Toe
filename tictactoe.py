b4b3ec3f09541c36fb0c35c7642f334a34f02b9d
herzanm <75642257+herzanm@users.noreply.github.com>
2023-10-27 16:37:40 +0200

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,170 +1,121 @@
-# write your code here
-user_input = list("         ")
+def print_grid(grid):
+    print("---------")
+    print(f"| {grid[0][0]} {grid[0][1]} {grid[0][2]} |")
+    print(f"| {grid[1][0]} {grid[1][1]} {grid[1][2]} |")
+    print(f"| {grid[2][0]} {grid[2][1]} {grid[2][2]} |")
+    print(f"---------")
 
-def outputDesk(user_input):
-        print("---------")
-        print(f"""| {user_input[0]} {user_input[1]} {user_input[2]} |
-| {user_input[3]} {user_input[4]} {user_input[5]} |
-| {user_input[6]} {user_input[7]} {user_input[8]} |""")
-        print("---------")
 
-#  Exceptions:
-def checkIfNumbers(user_move):
-    if ''.join(user_move).isdigit() == False:
-        print("You should enter numbers!")
-        return False
-    return True
+def move(grid, player):
+    while True:
+        user_input = input().split()
+        try:
+            if playground[int(user_input[0]) - 1][int(user_input[1]) - 1] != '_':
+                print("This cell is occupied! Choose another one!")
+            else:
+                playground[int(user_input[0]) - 1][int(user_input[1]) - 1] = player
+                if player == 'X':
+                    player = 'O'
+                else:
+                    player = 'X'
+                break
+        except ValueError:
+            print("You should enter numbers!")
+        except TypeError:
+            print("You should enter numbers!")
+        except IndexError:
+            print("Coordinates should be from 1 to 3!")
+            continue
 
-def checkIfInScope(user_move):
-    for x in user_move:
-        if int(x) < 1 or int(x) > 3:
-            print("Coordinates should be from 1 to 3")
-            return False
-    return True
 
-def coordinatesTransfer(user_move):
-    if "".join(user_move) == "11":
-        return 0
-    elif "".join(user_move) == "12":
-        return 1
-    elif "".join(user_move) == "13":
-        return 2
-    elif "".join(user_move) == "21":
-        return 3
-    elif "".join(user_move) == "22":
-        return 4
-    elif "".join(user_move) == "23":
-        return 5
-    elif "".join(user_move) == "31":
-        return 6
-    elif "".join(user_move) == "32":
-        return 7
-    elif "".join(user_move) == "33":
-        return 8
-
-def checkIfOccupied(position):
-    if user_input[position] == "X" or user_input[position] == "O":
-        print("This cell is occupied! Choose another one!")
-        return False
-    else:
-        return True
-
-#  Possible outcomes
-def X_winning(user_input):
-        #  Horizontal win
-        if user_input[0] == "X" and user_input[1] == "X" and user_input[2] == "X":
-                return True
-        elif user_input[3] == "X" and user_input[4] == "X" and user_input[5] == "X":
-                return True
-        elif user_input[6] == "X" and user_input[7] == "X" and user_input[8] == "X":
-                return True
-        #  Vertical win
-        elif user_input[0] == "X" and user_input[3] == "X" and user_input[6] == "X":
-                return True
-        elif user_input[1] == "X" and user_input[4] == "X" and user_input[7] == "X":
-                return True
-        elif user_input[2] == "X" and user_input[5] == "X" and user_input[8] == "X":
-                return True
-        #  Diagonal win
-        elif user_input[0] == "X" and user_input[4] == "X" and user_input[8] == "X":
-                return True
-        elif user_input[2] == "X" and user_input[4] == "X" and user_input[6] == "X":
-                return True
-        else:
-                return False
-
-def O_winning(user_input):
-        #  Horizontal win
-        if user_input[0] == "O" and user_input[1] == "O" and user_input[2] == "O":
-                return True
-        elif user_input[3] == "O" and user_input[4] == "O" and user_input[5] == "O":
-                return True
-        elif user_input[6] == "O" and user_input[7] == "O" and user_input[8] == "O":
-                return True
-        #  Vertical win
-        elif user_input[0] == "O" and user_input[3] == "O" and user_input[6] == "O":
-                return True
-        elif user_input[1] == "O" and user_input[4] == "O" and user_input[7] == "O":
-                return True
-        elif user_input[2] == "O" and user_input[5] == "O" and user_input[8] == "O":
-                return True
-        #  Diagonal win
-        elif user_input[0] == "O" and user_input[4] == "O" and user_input[8] == "O":
-                return True
-        elif user_input[2] == "O" and user_input[4] == "O" and user_input[6] == "O":
-                return True
-        else:
-                return False
-
-def draw(user_input):
-    count = 0
-    for x in user_input:
-        if x == 'X' or x == "O":
-            count += 1
-    if count == 9:
-            return True
-    else:
-            return False
-
-def impossible(user_input, X_winning, O_winning):
+def more_than_possible(grid):
     count_of_X = 0
     count_of_O = 0
-    for x in user_input:
-        if x == 'X':
+    for i in grid:
+        if i == 'X':
             count_of_X += 1
-
-        if x == 'O':
+        elif i == 'O':
             count_of_O += 1
-
-    difference = abs(count_of_O - count_of_X)
-    if difference >= 2:
-            print("Impossible")
-            return bool(True)
-    else:
-            if X_winning(user_input) == True and O_winning(user_input) == True:
-                        print("Impossible")
-                        return bool(True)
-            else:
-                        return bool(False)
-
-#  Going through possible outcomes
-def isItOver(user_input):
-    if impossible(user_input, X_winning, O_winning) == False:
-        if X_winning(user_input):
-            print("X wins")
-            return True
-        elif O_winning(user_input):
-            print("O wins")
-            return True
-        elif draw(user_input) == True:
-            print("Draw")
-            return True
-        else:
-            return False
-    else:
+    if abs(count_of_O - count_of_X) > 1:
         return True
-
-outputDesk(user_input)
-
-mark = "X"
-
-while isItOver(user_input) == False:
-    user_move = input("Enter the coordinates: ").split()
-    if checkIfNumbers(user_move) == False:
-        continue
     else:
-        if checkIfInScope(user_move) == False:
-            continue
-        else:
-            position = coordinatesTransfer(user_move)
-            if checkIfOccupied(position) == False:
-                continue
-            else:
-                user_input[position] = mark
-                outputDesk(user_input)
-                if mark == "X":
-                    mark = "O"
-                else:
-                    mark = "X"
+        return False
 
 
+def winner_x(grid):
+    if grid[0][0] == grid[0][1] == grid[0][2] == 'X':
+        return True
+    elif grid[1][0] == grid[1][1] == grid[1][2] == 'X':
+        return True
+    elif grid[2][0] == grid[2][1] == grid[2][2] == 'X':
+        return True
+    elif grid[0][0] == grid[1][0] == grid[2][0] == 'X':
+        return True
+    elif grid[0][1] == grid[1][1] == grid[2][1] == 'X':
+        return True
+    elif grid[0][2] == grid[1][2] == grid[2][2] == 'X':
+        return True
+    elif grid[0][0] == grid[1][1] == grid[2][2] == 'X':
+        return True
+    elif grid[2][0] == grid[1][1] == grid[0][2] == 'X':
+        return True
+    else:
+        return False
+
+
+def winner_o(grid):
+    if grid[0][0] == grid[0][1] == grid[0][2] == 'O':
+        return True
+    elif grid[1][0] == grid[1][1] == grid[1][2] == 'O':
+        return True
+    elif grid[2][0] == grid[2][1] == grid[2][2] == 'O':
+        return True
+    elif grid[0][0] == grid[1][0] == grid[2][0] == 'O':
+        return True
+    elif grid[0][1] == grid[1][1] == grid[2][1] == 'O':
+        return True
+    elif grid[0][2] == grid[1][2] == grid[2][2] == 'O':
+        return True
+    elif grid[0][0] == grid[1][1] == grid[2][2] == 'O':
+        return True
+    elif grid[2][0] == grid[1][1] == grid[0][2] == 'O':
+        return True
+    else:
+        return False
+
+
+def draw(grid):
+    if any([y == '_' for x in grid for y in x]):
+        return False
+    return True
+
+
+def check_for_end(grid):
+    if more_than_possible(grid):
+        print("Impossible")
+        exit(1)
+    elif winner_x(grid) and winner_o(grid):
+        print("Impossible")
+        exit(1)
+    elif winner_x(grid):
+        print("X wins")
+        exit(1)
+    elif winner_o(grid):
+        print("O wins")
+        exit(1)
+    elif draw(grid):
+        print("Draw")
+        exit(1)
+
+
+playground = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+turn = 'X'
+# main
+while True:
+    print_grid(playground)
+    check_for_end(playground)
+    move(playground, turn)
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
